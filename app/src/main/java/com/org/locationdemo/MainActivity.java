85e17c31df4280b36f4ae138518212d45100eb92
MacNew <newmac1000@gmail.com>
2019-03-14 20:20:48 +0545

    Notification channel created

--- app/src/main/java/com/org/locationdemo/MainActivity.java
+++ app/src/main/java/com/org/locationdemo/MainActivity.java
@@ -1,20 +1,28 @@
 package com.org.locationdemo;
 
 import android.Manifest;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
-
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
+import static com.org.locationdemo.App.CHANNEL_1_ID;
+import static com.org.locationdemo.App.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     private final int REQUEST_LOCATION_PERMISSION = 1;
 
@@ -22,9 +30,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager = NotificationManagerCompat.from(this);
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
+
+        setContentView(R.layout.activity_main);
 
         requestLocationPermission();
     }
+
+    public void sendOnchannel1(View view) {
+        Notification notification= new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_1)
+                .setContentTitle(editTextTitle.getText().toString())
+                .setContentText(editTextMessage.getText().toString())
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .build();
+        notificationManager.notify(1,notification);
+
+    }
+
+    public void sendOnchannel2(View view) {
+        Toast.makeText(this, editTextTitle.getText().toString(), Toast.LENGTH_SHORT).show();
+        Notification notification= new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_1)
+                .setContentTitle(editTextTitle.getText().toString())
+                .setContentText(editTextMessage.getText().toString())
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+        notificationManager.notify(2,notification);
+
+
+    }
+
+
 
     private void showSystemNotification() {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
